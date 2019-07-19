a1dd8b99f7374a2b639171d17dd8629961098213
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-07-19 17:57:26 +0200

    feat(js): Add box-shadow trnasitions on click events

---
+++ client/public/js/main.js
@@ -0,0 +1,17 @@
+const $selection = $('#selection');
+const $items = $('#selection .item');
+
+let width = $selection.css('width');
+let height = $selection.css('height');
+let maxExtension = Math.min(width, height);
+
+$selection.click(event => {
+    let dx = (Math.random() - 0.5) * 10;
+    let dy = (Math.random() - 0.5) * 10;
+    let alpha = Math.max(Math.random(), 0.5);
+
+    $items.each((index, element) => {
+        $(element).css('box-shadow',
+            `${dx}px ${dy}px 5px rgba(0, 0, 0, ${alpha})`);
+    });
+});
