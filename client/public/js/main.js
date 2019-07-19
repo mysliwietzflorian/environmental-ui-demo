370274766e0d451c28b32153a439cf14e687622f
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-07-19 18:23:27 +0200

    chore(js): Rename shadow directions to shadowX/shadowY

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -6,13 +6,13 @@ let height = $selection.css('height').replace(/[^-\d\.]/g, '');
 let maxExtension = Math.min(width, height);
 
 $selection.click(event => {
-    let dx = (Math.random() - 0.5) * 10;
-    let dy = (Math.random() - 0.5) * 10;
+    let shadowX = (Math.random() - 0.5) * 10;
+    let shadowY = (Math.random() - 0.5) * 10;
     // interpolate random between [0.1, 0.5]
     let alpha = Math.random() * (0.5 - 0.1) + 0.1;
 
     $items.each((index, element) => {
         $(element).css('box-shadow',
-            `${dx}px ${dy}px 5px rgba(0, 0, 0, ${alpha})`);
+            `${shadowX}px ${shadowY}px 5px rgba(0, 0, 0, ${alpha})`);
     });
 });
